33a1ed9cd2d0c742f2346cad036eadf6d96b8fc7
yuki_m <yakan.meroyan@gmail.com>
2019-08-07 00:01:54 +0900

    Create Top-View

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,5 @@
+class GenresController < ApplicationController
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'stores#index'
+  resources :stores , only: [:index]
+  resources :genres , only: [:index]
 end
