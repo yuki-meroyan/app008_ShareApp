5b388f3efb5038137dd1f9c7353df6e69f72b2a4
yuki_m <yakan.meroyan@gmail.com>
2019-08-22 00:15:05 +0900

    update view

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,21 +1,10 @@
 class StoresController < ApplicationController
-  before_action :set_genre, only: [:index]
 
   def index
-
+    @genres = Genre.all
   end
   
   def show
-  end
-
-  private
-  def set_genre
-    if params[:genre_id] == nil
-      @page_name = "ALL's PAGE"
-    else
-      genre = Genre.where(id: params[:genre_id])
-      @page_name = genre[0].name + "'s PAGE"
-    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'genres#index'
+  root to: 'stores#index'
   resources :users,   only: [:index, :edit, :update]
   resources :genres , only: [:index, :show]
   resources :details
